e46da44c49a31a963f723fdd523fdc206158e433
priya <priya@zawqwe.com>
2023-06-16 10:47:52 +0530

    moviecatalog hard code data

--- MovieCatalogService/src/main/java/com/example/jpaConceptDemo/MovieCatalogResource.java
+++ MovieCatalogService/src/main/java/com/example/jpaConceptDemo/MovieCatalogResource.java
@@ -1,10 +1,22 @@
 package com.example.jpaConceptDemo;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieCatalogResource {
 
-	public List<Catalog> getCatalog(String uid){
-		}
+	
+	@GetMapping("/catalog/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable String userId){
+
+		return Collections.singletonList(new CatalogItem("spiderman","saving galaxt",4)
+				);
+
+		//return? movie details along with ratings
 	}
+}
