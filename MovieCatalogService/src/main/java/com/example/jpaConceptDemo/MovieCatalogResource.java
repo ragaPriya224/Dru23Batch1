5b0c90c11b1b80f7d8f5cb4ed5545dadab92f497
priya <priya@zawqwe.com>
2023-06-16 10:19:57 +0530

    intial3 repo commit

---
+++ MovieCatalogService/src/main/java/com/example/jpaConceptDemo/MovieCatalogResource.java
@@ -0,0 +1,10 @@
+package com.example.jpaConceptDemo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieCatalogResource {
+
+	public List<Catalog> getCatalog(String uid){
+		}
+	}
---
+++ RatingDataservice/src/main/java/com/example/jpaConceptDemo/RatingDataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaConceptDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingDataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingDataserviceApplication.class, args);
+	}
+
+}
