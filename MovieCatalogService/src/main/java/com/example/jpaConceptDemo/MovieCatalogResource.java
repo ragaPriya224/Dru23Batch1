1c89101450669a9294824fd6e02b3e2d0e74cf75
priya <priya@zawqwe.com>
2023-06-16 16:36:03 +0530

    MOVIE API CALL

--- MovieCatalogService/src/main/java/com/example/jpaConceptDemo/MovieCatalogResource.java
+++ MovieCatalogService/src/main/java/com/example/jpaConceptDemo/MovieCatalogResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MovieCatalogResource {
@@ -20,6 +21,8 @@ public class MovieCatalogResource {
 		List<Rating> ratings = Arrays.asList(new Rating("222",4),
 				new Rating("41",5));
 
+		RestTemplate rt = new RestTemplate();
+	Movie	a =rt.getForObject("https://localhost:8082/movie/4123", Movie.class);
 		return ratings.stream().map(rating ->{
 			return new CatalogItem("Transformer","test",4);
 			}).collect(Collectors.toList());
